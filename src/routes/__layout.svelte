b0ead0d89414bfb2e43cf46c091debe200f4675e
Patrick Haun <patrick@bomgar.email>
2022-07-21 13:01:20 +0200

    initial commit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,7 @@
+<slot />
+
+<style global>
+	html {
+		font-family: 'Roboto', sans-serif;
+	}
+</style>
---
+++ src/routes/register.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { default as Button } from '@smui/button';
+	function register() {
+		console.info('register');
+	}
+</script>
+
+<h1>Register</h1>
+
+<form on:submit|preventDefault={register}>
+	<Button type="submit" variant="raised">Register</Button>
+</form>
