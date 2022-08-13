96f6a781dca194b38029da94646ef87632ffd992
Patrick Haun <patrick@bomgar.email>
2022-08-13 12:10:41 +0200

    add topbar

--- src/routes/__layout-protected.svelte
+++ src/routes/__layout-protected.svelte
@@ -13,4 +13,25 @@
   };
 </script>
 
-<slot />
+<script lang="ts">
+  import type { TopAppBarComponentDev } from "@smui/top-app-bar";
+  import TopAppBar, { Row, Section, Title, AutoAdjust } from "@smui/top-app-bar";
+  import IconButton from "@smui/icon-button";
+
+  let topAppBar: TopAppBarComponentDev;
+</script>
+
+<TopAppBar bind:this={topAppBar} variant="fixed">
+  <Row>
+    <Section>
+      <!--<IconButton class="material-icons">menu</IconButton> -->
+      <Title>Status Dog</Title>
+    </Section>
+    <Section align="end" toolbar>
+      <IconButton class="material-icons" aria-label="Bookmark this page">logout</IconButton>
+    </Section>
+  </Row>
+</TopAppBar>
+<AutoAdjust {topAppBar}>
+  <slot />
+</AutoAdjust>
